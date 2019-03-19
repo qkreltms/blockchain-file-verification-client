2f525793af87924be1e4b68b1f0e6aee10523ed2
JungHoon Park <wfr112@naver.com>
2019-03-19 17:50:28 +0900

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import { MainPage } from './components/mainPage'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <MainPage></MainPage>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/mainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export class MainPage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (<div> mainpage </div>)
+  }
+}
